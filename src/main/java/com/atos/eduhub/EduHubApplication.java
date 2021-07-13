47f3a6dc7daff6d171435e29ef5966fc77c07b4e
ChandravadhanaTK <tkchandravadhana@gmail.com>
2021-07-13 13:06:26 +0530

    Initial Commit - Eduhub - Backend Framework REST API Development

---
+++ src/main/java/com/atos/eduhub/EduHubApplication.java
@@ -0,0 +1,22 @@
+package com.atos.eduhub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+@ComponentScan({"com.atos.eduhub.controller","com.atos.eduhub.service"})
+public class EduHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EduHubApplication.class, args);
+	}
+
+	@RequestMapping(value="/")
+	public String hello() {
+		return "<h1> Hello All </h1>";
+	}
+}
