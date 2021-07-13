06c8f6cc5595904ff148737f31a5be9490749aa8
ChandravadhanaTK <tkchandravadhana@gmail.com>
2021-07-13 00:36:26 -0700

    added view 1 for Assignment

---
+++ src/test/java/com/atos/eduhub/EduHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atos.eduhub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
