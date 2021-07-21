37f82cd32572629fb8b1827a79038f724aeeaef9
MandarFSD <sqlcode805@gmail.com>
2021-07-21 11:05:29 +0100

    Added code code view one and all approvals

--- src/main/java/com/atos/eduhub/service/ApprovalService.java
+++ src/main/java/com/atos/eduhub/service/ApprovalService.java
@@ -1,13 +1,15 @@
 package com.atos.eduhub.service;
 
+import java.util.List;
+
 import com.atos.eduhub.model.Approval;
 
 public interface ApprovalService {
 	public Approval addApproval(Approval approval);
 	public Approval editApproval(Approval approval);
 	public void deleteApproval();
-	public void viewApproval();
-	public void viewAllApprovals();
+	public Approval viewOneApproval(int approvalId);
+	public List<Approval> viewAllApprovals();
 	public String deleteAllApprovals();
 
 }
