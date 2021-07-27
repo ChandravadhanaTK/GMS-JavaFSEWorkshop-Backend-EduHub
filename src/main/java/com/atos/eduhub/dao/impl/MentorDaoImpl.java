ef05b78fd33aed9108c5eb78f57110cb46ae6887
Inbaraj <ainbara@SYNTELINC.COM>
2021-07-27 09:54:35 +0530

    Removed JdbcDaoSupport class and its related methods in DAOImpl

--- src/main/java/com/atos/eduhub/dao/impl/MentorDaoImpl.java
+++ src/main/java/com/atos/eduhub/dao/impl/MentorDaoImpl.java
@@ -3,65 +3,47 @@ package com.atos.eduhub.dao.impl;
 import java.sql.Timestamp;
 import java.time.LocalDateTime;
 
-import javax.annotation.PostConstruct;
-import javax.sql.DataSource;
-
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.jdbc.core.JdbcTemplate;
-import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
 
 import com.atos.eduhub.dao.MentorDao;
 import com.atos.eduhub.model.Mentor;
 
 @Repository
-public class MentorDaoImpl extends JdbcDaoSupport implements MentorDao{
-	
+public class MentorDaoImpl implements MentorDao {
+
 	@Autowired
-	private DataSource datasource;
-	
-	//@Autowired
-	//private JdbcTemplate jdbctemplate;
+	private JdbcTemplate jdbctemplate;
 
 	public MentorDaoImpl() {
 		// TODO Auto-generated constructor stub
 	}
-	
-    @PostConstruct
-    private void initialize(){
-        setDataSource(datasource);
-    }
-	
+
 	@Override
-	public int addMentor(Mentor mentor){ 
-		
-		String sql 	=	"Insert into public.mentor (userid, startdatetime, enddatetime, mentoringSkill, " 
-		 +	" mentoredHours, mentorRating, aboutMentor, last_updated_on) values (?, ?, ?, ?, ?, ?, ?, ?)";
-		
-		return getJdbcTemplate().update(sql,
-										mentor.getUserid(),
-										mentor.getStartdatetime(),
-										mentor.getEnddatetime(),
-										mentor.getMentoringskill(),
-										mentor.getMentoredhours(),
-										mentor.getMentorrating(),
-										mentor.getAboutmentor(),
-										Timestamp.valueOf(LocalDateTime.now()));
+	public int addMentor(Mentor mentor) {
+
+		String sql = "Insert into public.mentor (userid, startdatetime, enddatetime, mentoringSkill, "
+				+ " mentoredHours, mentorRating, aboutMentor, last_updated_on) values (?, ?, ?, ?, ?, ?, ?, ?)";
+
+		return jdbctemplate.update(sql, mentor.getUserid(), mentor.getStartdatetime(), mentor.getEnddatetime(),
+				mentor.getMentoringskill(), mentor.getMentoredhours(), mentor.getMentorrating(),
+				mentor.getAboutmentor(), Timestamp.valueOf(LocalDateTime.now()));
 	}
 
 	@Override
-	public int deleteAllMentors(){ 
+	public int deleteAllMentors() {
 
-		String sql 	=	"DELETE FROM public.mentor";
-		
-		return getJdbcTemplate().update(sql);
+		String sql = "DELETE FROM public.mentor";
+
+		return jdbctemplate.update(sql);
 	}
-	
-	@Override
-	public int deleteMentor(int mentorId){ 
 
-		String sql 	=	"DELETE FROM public.mentor WHERE userid=?";
-		
-		return getJdbcTemplate().update(sql,mentorId);
+	@Override
+	public int deleteMentor(int mentorId) {
+
+		String sql = "DELETE FROM public.mentor WHERE userid=?";
+
+		return jdbctemplate.update(sql, mentorId);
 	}
 }
--- src/main/java/com/atos/eduhub/service/impl/MentorServiceImpl.java
+++ src/main/java/com/atos/eduhub/service/impl/MentorServiceImpl.java
@@ -18,33 +18,22 @@ public class MentorServiceImpl implements MentorService {
 
 	@Autowired
 	private Mentor mentor;
-	
+
 	@Autowired
 	private MentorDaoImpl mentordaoimpl;
-	
+
 	@Override
 	public String addMentor(@RequestBody Mentor mentorreq) {
-		
-		return mentordaoimpl.addMentor(mentorreq) > 0 
+
+		return mentordaoimpl.addMentor(mentorreq) > 0
 				? "Mentor " + mentorreq.getUserid() + " has been added to EduHub successfully"
 				: "Issue with adding mentor " + mentorreq.getUserid();
-		
-//		mentor.setLastupdatedon();
-//		mentor.setUserid(mentorreq.getUserid());
-//		mentor.setEnddatetime(mentorreq.getEnddatetime());
-//		mentor.setAboutmentor(mentorreq.getAboutmentor());
-//		mentor.setMentorrating(mentorreq.getMentorrating());
-//		mentor.setStartdatetime(mentorreq.getStartdatetime());
-//		mentor.setMentoredhours(mentorreq.getMentoredhours());
-//		mentor.setAvailabilityid(mentorreq.getAvailabilityid());
-//		mentor.setMentoringskill(mentorreq.getMentoringskill());	
-//		System.out.println(mentorreq);
-		
+
 	}
 
 	@Override
 	public String editMentor(@RequestBody Mentor mentorreq) {
-	
+
 		mentor.setLastupdatedon();
 		mentor.setUserid(mentorreq.getUserid());
 		mentor.setEnddatetime(mentorreq.getEnddatetime());
@@ -54,11 +43,11 @@ public class MentorServiceImpl implements MentorService {
 		mentor.setMentoredhours(mentorreq.getMentoredhours());
 		mentor.setAvailabilityid(mentorreq.getAvailabilityid());
 		mentor.setMentoringskill(mentorreq.getMentoringskill());
-	
+
 		System.out.println(mentor);
 		return "Mentor " + mentorreq.getUserid() + " has been updated successfully";
 	}
-	
+
 	@Override
 	public Mentor viewMentor() {
 
@@ -71,16 +60,16 @@ public class MentorServiceImpl implements MentorService {
 		mentor.setMentorrating("5");
 		mentor.setAboutmentor("Java mentor");
 		mentor.setLastupdatedon();
-		
+
 		return mentor;
 	}
-	
+
 	@Override
 	public List<Mentor> viewAllMentors() {
 		// TODO Auto-generated method stub
-		
+
 		List<Mentor> getMentors = new ArrayList<Mentor>();
-		
+
 		Mentor mentor1 = new Mentor();
 		mentor1.setAvailabilityid(100);
 		mentor1.setUserid(1000);
@@ -91,9 +80,9 @@ public class MentorServiceImpl implements MentorService {
 		mentor1.setMentorrating("5");
 		mentor1.setAboutmentor("Java mentor");
 		mentor1.setLastupdatedon();
-		
+
 		getMentors.add(mentor1);
-		
+
 		Mentor mentor2 = new Mentor();
 		mentor2.setAvailabilityid(101);
 		mentor2.setUserid(2000);
@@ -104,24 +93,22 @@ public class MentorServiceImpl implements MentorService {
 		mentor2.setMentorrating("4");
 		mentor2.setAboutmentor("Python mentor");
 		mentor2.setLastupdatedon();
-		
+
 		getMentors.add(mentor2);
 		return getMentors;
 	}
-	
+
 	@Override
 	public String deleteMentor(int id) {
-		return mentordaoimpl.deleteMentor(id) > 0 
-				? "Mentor with userid " + id + " was deleted from EduHub successfully"
+		return mentordaoimpl.deleteMentor(id) > 0 ? "Mentor with userid " + id + " was deleted from EduHub successfully"
 				: "Issue with deleting mentor " + id;
 	}
-	
+
 	@Override
 	public String deleteAllMentors() {
 		int deletedMentorsCount;
-		deletedMentorsCount=mentordaoimpl.deleteAllMentors();
-		return deletedMentorsCount > 0 
-				? deletedMentorsCount + " mentors were deleted from EduHub successfully"
+		deletedMentorsCount = mentordaoimpl.deleteAllMentors();
+		return deletedMentorsCount > 0 ? deletedMentorsCount + " mentors were deleted from EduHub successfully"
 				: "Issue with deleting mentors";
 	}
 }
